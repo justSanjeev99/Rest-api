3a55c932b45eef374666b7886a0536a7aaef0ebe
Sanjeev Mishra <mishragsanjeev1999@gmail.com>
2022-03-17 03:04:41 +0530

    Update server.js

--- resthub/server.js
+++ resthub/server.js
@@ -5,7 +5,7 @@ let express = require('express'),
     bodyParser = require('body-parser'),
     dbConfig = require('./db/database');
 
-// Connecting mongoDB
+
 mongoose.Promise = global.Promise;
 mongoose.connect(dbConfig.db, {
     useNewUrlParser: true
@@ -16,31 +16,27 @@ mongoose.connect(dbConfig.db, {
         console.log('Database could not be connected : ' + error)
     }
 )
-// Setting up express
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(cors());
-// Api root
 const userRoute = require('./routes/student.route')
 app.use('/endpoint', userRoute)
-// Create port
 const port = process.env.PORT || 8080;
-// Conectting port
+
 const server = app.listen(port, () => {
     console.log('Port connected to: ' + port)
 })
-// Find 404 and hand over to error handler
 app.use((req, res, next) => {
     next(createError(404));
 });
-// Index Route
+
 app.get('/', (req, res) => {
     res.send('invaild endpoint');
 });
-// error handler
 app.use(function (err, req, res, next) {
     console.error(err.message);
     if (!err.statusCode) err.statusCode = 500;
